7a9272f371dc73777f64a65f1e61e6d5b6cd1a65
Nedim <nedim_nemesis@hotmail.de>
2013-08-06 23:40:06 +0200

    test123

--- projectRPG/src/com/example/projectrpg/MainActivity.java
+++ projectRPG/src/com/example/projectrpg/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	//TEST-Kommentar... kann danach wieder gelöscht werden
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
