9797c96470022a63f88ec0bb9cce972421e6d8f8
Nedim <nedim_nemesis@hotmail.de>
2013-08-09 18:55:11 +0200

    testmenu

--- projectRPG/gen/com/example/projectrpg/R.java
+++ projectRPG/gen/com/example/projectrpg/R.java
@@ -24,7 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Tabelle=0x7f080001;
+        public static final int action_settings=0x7f080008;
+        public static final int help_textview=0x7f080007;
+        public static final int load_game_textview=0x7f080004;
+        public static final int new_game_textview=0x7f080003;
+        public static final int options_textview=0x7f080006;
+        public static final int row_one=0x7f080002;
+        public static final int row_two=0x7f080005;
+        public static final int title_textview=0x7f080000;
     }
     public static final class layout {
         public static final int activity_level=0x7f030000;
@@ -37,7 +45,16 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int description_help=0x7f050008;
+        public static final int description_load_game=0x7f050006;
+        public static final int description_options=0x7f050007;
+        public static final int description_start_game=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int help_string=0x7f05000c;
+        public static final int load_game_string=0x7f05000a;
+        public static final int new_game_string=0x7f050009;
+        public static final int options_string=0x7f05000b;
+        public static final int startmenu_title=0x7f050004;
         public static final int title_activity_level=0x7f050003;
     }
     public static final class style {
--- projectRPG/src/com/example/projectrpg/MainActivity.java
+++ projectRPG/src/com/example/projectrpg/MainActivity.java
@@ -2,16 +2,47 @@ package com.example.projectrpg;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
-	//TEST-Kommentar... kann danach wieder gelöscht werden
+	private Typeface inhishan;
+
+	private TextView title;
+	private TextView newGame;
+	private TextView loadGame;
+	private TextView options;
+	private TextView help;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		setupUI();
+	}
+
+	private void setupUI() {
 		setContentView(R.layout.activity_main);
+		
+		inhishan = Typeface
+				.createFromAsset(getAssets(), "fonts/inhishan.ttf");
+
+		title = (TextView)findViewById(R.id.title_textview);
+		title.setTypeface(inhishan);
+		
+		newGame = (TextView)findViewById(R.id.new_game_textview);
+		newGame.setTypeface(inhishan);
+		
+		loadGame = (TextView)findViewById(R.id.load_game_textview);
+		loadGame.setTypeface(inhishan);
+		
+		options = (TextView)findViewById(R.id.options_textview);
+		options.setTypeface(inhishan);
+		
+		help = (TextView)findViewById(R.id.help_textview);
+		help.setTypeface(inhishan);
 	}
 
 	@Override
