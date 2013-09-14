7c6e428385362e09766dfc339bf668a2780a32f6
Nedim <nedim_nemesis@hotmail.de>
2013-09-14 18:08:38 +0200

    mainmenu background bug gefixt; Menü gedeutscht^^; Texte im Menü auch clickable; Slots auch erstellt, jedoch bisher mit TEST-background & TEST-Text (linker Slot startet bisher Spiel neu, rechter Slot lädt bisher das gespeicherte Spiel

--- projectRPG/src/de/projectrpg/start/MainActivity.java
+++ projectRPG/src/de/projectrpg/start/MainActivity.java
@@ -14,7 +14,9 @@ import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.projectrpg.R;
 
@@ -28,20 +30,24 @@ public class MainActivity extends Activity {
 
 	private TextView title;
 	private TextView newGame;
-	private TextView loadGame;
+	private TextView savedGameSlot1;
+	private TextView savedGameSlot2;
 	private TextView options;
 	private TextView help;
 
 	private ImageView newGameImageView;
-	private ImageView loadGameImageView;
 	private ImageView optionsImageView;
 	private ImageView helpImageView;
 
 	private HorizontalScrollView parallaxMidLayer;
 	private HorizontalScrollView parallaxFrontLayer;
+	
+	private TableLayout table;
 
 	private int scrolled1 = 0;
 	private int scrolled2 = 0;
+	
+	private boolean isBackButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -54,59 +60,14 @@ public class MainActivity extends Activity {
 	}
 
 	private void setupClickListener() {
-		newGameImageView.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(MainActivity.this, LevelActivity.class);
-				i.putExtra("slot", 1);
-				i.putExtra("newGame", true);
-				startActivity(i);
-			}
-		});
-
-		loadGameImageView.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(MainActivity.this, LevelActivity.class);
-				i.putExtra("slot", 1);
-				i.putExtra("newGame", false);
-				startActivity(i);
-			}
-		});
-
-		optionsImageView.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-
-			}
-		});
-
-		helpImageView.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, HelpActivity.class);
-				startActivity(intent);
-			}
-		});
-
-		parallaxMidLayer.setOnTouchListener(new OnTouchListener() {
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				return true;
-			}
-		});
-
-		parallaxFrontLayer.setOnTouchListener(new OnTouchListener() {
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				return true;
-			}
-		});
+		setOnClickListenerStartGame(newGameImageView);
+		setOnClickListenerStartGame(newGame);
+		setOnClickListenerOptions(optionsImageView);
+		setOnClickListenerOptions(options);
+		setOnClickListenerHelp(helpImageView);
+		setOnClickListenerHelp(help);
+		deactivateScrollingbyUser(parallaxMidLayer);
+		deactivateScrollingbyUser(parallaxFrontLayer);
 	}
 
 	private void setupUI() {
@@ -120,8 +81,13 @@ public class MainActivity extends Activity {
 		newGame = (TextView) findViewById(R.id.new_game_textview);
 		newGame.setTypeface(inhishan);
 
-		loadGame = (TextView) findViewById(R.id.load_game_textview);
-		loadGame.setTypeface(inhishan);
+		savedGameSlot1 = (TextView) findViewById(R.id.saved_game_slot1);
+		savedGameSlot1.setTypeface(inhishan);
+		savedGameSlot1.setVisibility(View.INVISIBLE);
+
+		savedGameSlot2 = (TextView) findViewById(R.id.saved_game_slot2);
+		savedGameSlot2.setTypeface(inhishan);
+		savedGameSlot2.setVisibility(View.INVISIBLE);
 
 		options = (TextView) findViewById(R.id.options_textview);
 		options.setTypeface(inhishan);
@@ -130,11 +96,101 @@ public class MainActivity extends Activity {
 		help.setTypeface(inhishan);
 
 		newGameImageView = (ImageView) findViewById(R.id.new_game_imageview);
-		loadGameImageView = (ImageView) findViewById(R.id.load_game_imageview);
 		optionsImageView = (ImageView) findViewById(R.id.options_imageview);
 		helpImageView = (ImageView) findViewById(R.id.help_imageview);
 		parallaxMidLayer = (HorizontalScrollView) findViewById(R.id.scroll);
 		parallaxFrontLayer = (HorizontalScrollView) findViewById(R.id.scroll2);
+		
+		table = (TableLayout) findViewById(R.id.Tabelle);
+		
+	}
+	
+	private void setOnClickListenerStartGame(View view){
+		view.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				table.setVisibility(View.INVISIBLE);
+				newGame.setVisibility(View.INVISIBLE);
+				newGameImageView.setVisibility(View.INVISIBLE);
+				savedGameSlot1.setVisibility(View.VISIBLE);
+				savedGameSlot2.setVisibility(View.VISIBLE);
+				setOnClickListenerNewGame(savedGameSlot1);
+				setOnClickListenerLoadGame(savedGameSlot2);
+				isBackButton = true;
+			}
+		});
+	}
+	
+	@Override
+	public void onBackPressed() {
+		if(isBackButton){
+			table.setVisibility(View.VISIBLE);
+			newGame.setVisibility(View.VISIBLE);
+			newGameImageView.setVisibility(View.VISIBLE);
+			savedGameSlot1.setVisibility(View.INVISIBLE);
+			savedGameSlot2.setVisibility(View.INVISIBLE);
+			isBackButton = false;
+		} else {
+			finish();
+		}
+	}
+	
+	private void setOnClickListenerNewGame(View view){
+		view.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(MainActivity.this, LevelActivity.class);
+				i.putExtra("slot", 1);
+				i.putExtra("newGame", true);
+				startActivity(i);
+			}
+		});
+	}
+	
+	private void setOnClickListenerLoadGame(View view){
+		view.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(MainActivity.this, LevelActivity.class);
+				i.putExtra("slot", 1);
+				i.putExtra("newGame", false);
+				startActivity(i);
+			}
+		});
+	}
+	
+	private void setOnClickListenerOptions(View view){
+		view.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+	}
+	
+	private void setOnClickListenerHelp(View view){
+		view.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, HelpActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+	
+	private void deactivateScrollingbyUser(View view){
+		view.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				return true;
+			}
+		});
 	}
 
 	class MyAsyncClouds extends AsyncTask<String, String, String> {
@@ -172,8 +228,8 @@ public class MainActivity extends Activity {
 						public void run() {
 							parallaxMidLayer.scrollBy(1, 0);
 							scrolled1++;
-							if (scrolled1 == 1280) {
-								parallaxMidLayer.scrollBy(-1280, 0);
+							if (scrolled1 == 1920) {
+								parallaxMidLayer.scrollBy(-1920, 0);
 								scrolled1 = 0;
 							}
 						}
@@ -181,7 +237,6 @@ public class MainActivity extends Activity {
 				}
 			}, 0, 45);
 		}
-
 	}
 
 	class MyAsyncGround extends AsyncTask<String, String, String> {
@@ -219,8 +274,8 @@ public class MainActivity extends Activity {
 						public void run() {
 							parallaxFrontLayer.scrollBy(2, 0);
 							scrolled2 = scrolled2 + 2;
-							if (scrolled2 == 1280) {
-								parallaxFrontLayer.scrollBy(-1280, 0);
+							if (scrolled2 == 1920) {
+								parallaxFrontLayer.scrollBy(-1920, 0);
 								scrolled2 = 0;
 							}
 						}
@@ -230,6 +285,5 @@ public class MainActivity extends Activity {
 		}
 
 	}
-
 }
 
