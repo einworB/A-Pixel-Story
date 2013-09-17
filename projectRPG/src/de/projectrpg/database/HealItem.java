2c8fe3d8054e2af80959c1edcdd10641e64a472b
Lena <Lena@UNIversum.fritz.box>
2013-09-17 22:34:31 +0200

    Kommentiert

--- projectRPG/src/de/projectrpg/database/HealItem.java
+++ projectRPG/src/de/projectrpg/database/HealItem.java
@@ -2,13 +2,25 @@ package de.projectrpg.database;
 
 public class HealItem extends Item {
 	
+	/** the heal value of this item*/
 	private int healValue;
 
+	/**
+	 * the constructor
+	 * @param name the name of this item
+	 * @param levelNeeded the level the player needs to eat this item
+	 * @param itemType the typ of item
+	 * @param healValue the heal value of this item
+	 */
 	public HealItem(String name, int levelNeeded, String itemType, int healValue) {
 		super(name, levelNeeded, itemType);
 		this.healValue = healValue;
 	}
 
+	/**
+	 * get the heal value
+	 * @return the heal value
+	 */
 	public int getHeal() {
 		return healValue;
 	}
